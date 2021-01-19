aecf077d9d9aa1b95590295f782866d6cf956e92
Roberto Miguez <miguezroberto@gmail.com>
2021-01-19 15:47:27 +0000

    upgrade clipboard package

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCopy, faEdit, faTrashAlt, faPlusSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import VueClipboard from 'vue-clipboard2'
+import VueClipboard from 'vue-clipboard-plus'
 
 library.add(faTwitter, faLinkedin, faCopy, faEdit, faTrashAlt, faPlusSquare)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
